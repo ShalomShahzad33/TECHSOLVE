983f35ead2f883565f6e8f818771a320f9006bf0
ShalomShahzad33 <shalomshahzad450@gmail.com>
2025-12-28 12:18:41 +0500

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-      <div>App</div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
